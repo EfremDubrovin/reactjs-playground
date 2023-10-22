65c8f87e74a6004c8169a8a0a138f7b3d8741a3e
Efrem Dubrovin <efremdubrovin@deltasource.eu>
2023-10-22 21:06:14 +0300

    add table example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import CounterComponent from "./components/counter/CounterComponent";
+import MovieTable from "./components/table/MovieTable";
 
 function App() {
-  return <CounterComponent />;
+  return (
+    <>
+      <CounterComponent />
+      <MovieTable
+        movieItems={[
+          {
+            movieName: "Matricata",
+            episodeNumber: 1,
+            isAvailableForDownload: true,
+          },
+          {
+            movieName: "Столичани в повече",
+            episodeNumber: 3,
+            isAvailableForDownload: false,
+          },
+        ]}
+      />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/table/MovieTable.tsx
@@ -0,0 +1,36 @@
+import { FcOk, FcCancel } from "react-icons/fc";
+
+interface MovieItem {
+  movieName: string;
+  episodeNumber: number;
+  isAvailableForDownload: boolean;
+}
+
+interface Props {
+  movieItems: MovieItem[];
+}
+
+function MovieTable({ movieItems }: Props) {
+  return (
+    <>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Episode number</th>
+          <th>AvailableForDownload</th>
+        </tr>
+        {movieItems.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.movieName}</td>
+              <td>{val.episodeNumber}</td>
+              <td>{val.isAvailableForDownload ? <FcOk/> : <FcCancel/>}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </>
+  );
+}
+
+export default MovieTable;
